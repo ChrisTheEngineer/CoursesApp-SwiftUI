2842a23885565e4333027a932a3513379daa306a
Christian Ruiz <dream@syntheticfox.com>
2020-02-05 16:27:30 -0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Christian Ruiz on 2/5/20.
+//  Copyright © 2020 Synthetic Fox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
