8133af1378425e9165c25fb602d80a20f779fb5a
Christian Ruiz <dream@syntheticfox.com>
2020-02-10 21:00:31 -0800

    Navigation view and List

--- DesignCode/HomeView.swift
+++ DesignCode/HomeView.swift
@@ -33,7 +33,7 @@ struct HomeView: View {
                         .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
                 }
                 .sheet(isPresented: $showUpdate) { //brings up modal window
-                    ContentView()
+                    UpdateList()
                 }
             }
             .padding(.horizontal)
