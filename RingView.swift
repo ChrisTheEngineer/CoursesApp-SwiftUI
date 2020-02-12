348e96e4b605f649f947d0357a04f8968f1aef47
Christian Ruiz <dream@syntheticfox.com>
2020-02-12 00:00:28 -0800

    Ring Process Circle

---
+++ RingView.swift
@@ -0,0 +1,40 @@
+//
+//  RingView.swift
+//  DesignCode
+//
+//  Created by Christian Ruiz on 2/11/20.
+//  Copyright © 2020 Synthetic Fox. All rights reserved.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    var body: some View {
+        ZStack {
+            Circle() //inactive circle, to complete the cirlce
+                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5))
+                .frame(width: 44, height: 44)
+            
+            Circle()
+                .trim(from: 0.2, to: 1)
+                .stroke(
+                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), Color(#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1))]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20,0], dashPhase: 0)
+            ) //dash [20,20] gives a dashed circle
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                .frame(width: 44, height: 44)
+                .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.1), radius: 3, x: 0, y: 3)
+            
+            Text("82%")
+                .font(.subheadline)
+                .fontWeight(.bold)
+        }
+    }
+}
+
+struct RingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RingView()
+    }
+}
