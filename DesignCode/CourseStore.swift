e778418640ed83f6ad0e7fcb0e21a1085b7ed29c
Christian Ruiz <dream@syntheticfox.com>
2020-02-17 23:42:39 -0800

    URL Images with 3rd party lib, random card colors

--- DesignCode/CourseStore.swift
+++ DesignCode/CourseStore.swift
@@ -31,14 +31,16 @@ class CourseStore: ObservableObject {
     @Published var courses: [Course] = courseData
     
     init() {
+        let colors = [#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)]
+        
         getArray(id: "course") { (items) in
             items.forEach { (item) in
                 self.courses.append(Course(
                     title: item.fields["title"] as! String,
                     subtitle: item.fields["subtitle"] as! String,
-                    image: #imageLiteral(resourceName: "Background1"),
+                    image: item.fields.linkedAsset(at: "image")?.url ?? URL(string: "")!, //default value used
                     logo: #imageLiteral(resourceName: "Logo1"),
-                    color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                    color: colors.randomElement()!, //! to verify it's a color
                     show: false)
                 )
             }
