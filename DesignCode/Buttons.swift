d14584fd4de90a474abde0e0a107dfd86998589f
Christian Ruiz <christheengineer@icloud.com>
2020-07-08 23:09:04 -0600

    Tap progress animation - circle button

--- DesignCode/Buttons.swift
+++ DesignCode/Buttons.swift
@@ -15,6 +15,8 @@ struct Buttons: View {
             RectangleButton()
             
             CircleButton()
+            
+            PayButton()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.9121050239, green: 0.865039587, blue: 1, alpha: 1)))
@@ -124,16 +126,82 @@ struct CircleButton: View {
             .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.8195130229, green: 0.6300581098, blue: 0.8301144242, alpha: 1)), radius: 20, x: 20, y: 20)
             .scaleEffect(tap ? 1.2 : 1)
             .gesture(
-                    LongPressGesture(minimumDuration: 0.5, maximumDistance: 10).onChanged { value in
-                        self.tap = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { //timer
-                            self.tap = false
-                        }
-                        
+                LongPressGesture(minimumDuration: 0.5, maximumDistance: 10).onChanged { value in
+                    self.tap = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { //timer
+                        self.tap = false
                     }
-                    .onEnded { value in
-                        self.press.toggle()
-                    }
+                    
+                }
+                .onEnded { value in
+                    self.press.toggle()
+                }
+        )
+    }
+}
+
+struct PayButton: View {
+    @GestureState var tap = false
+    @State var press = false //long press
+    
+    var body: some View {
+        ZStack {
+            Image("fingerprint")
+                .opacity(press ? 0 : 1)
+                .scaleEffect(press ? 0 : 1)
+            
+            Image("fingerprint-2")
+                .clipShape(Rectangle().offset(y: tap ? 0 : 50))
+                .animation(.easeInOut) // no bounce back
+                .opacity(press ? 0 : 1)
+                .scaleEffect(press ? 0 : 1)
+            
+            
+            Image(systemName: "checkmark.circle.fill")
+                .font(.system(size: 44, weight: .light))
+                .foregroundColor(Color(#colorLiteral(red: 0.6530441642, green: 0.3625520468, blue: 1, alpha: 1)))
+                .opacity(press ? 1 : 0)
+                .scaleEffect(press ? 1 : 0)
+            
+            
+        }
+        .frame(width: 120, height: 120)
+        .background(
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.9121050239, green: 0.865039587, blue: 1, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.9121050239, green: 0.865039587, blue: 1, alpha: 1) )]), startPoint: .topLeading, endPoint: .bottomTrailing) //secondary color is lighter / more subtle than below shadow secondary colors
+                
+                Circle()
+                    .stroke(Color.clear, lineWidth: 10) //creates a border that's clear
+                    .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.8195130229, green: 0.6300581098, blue: 0.8301144242, alpha: 1)), radius: 3, x: -5, y: -5)
+                
+                Circle()
+                    .stroke(Color.clear, lineWidth: 10)
+                    .shadow(color: Color(press ? #colorLiteral(red: 0.8195130229, green: 0.6300581098, blue: 0.8301144242, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 3, x: 3, y: 3)
+            }
+        )
+            .clipShape(Circle())
+            .overlay(//progress bar
+                Circle()
+                    .trim(from: tap ? 0.001 : 1, to: 1) //animates progress circle
+                    .stroke(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.6530441642, green: 0.3625520468, blue: 1, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 5, lineCap: .round)) //creates cirlce border
+                    .frame(width: 88, height: 88) //shink
+                    .rotationEffect(Angle(degrees: 90)) //start progress from top by rotating
+                    .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                    .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 5, x: 3, y: 3)
+                    .animation(.easeInOut) // no bounce back
+                
             )
+            .shadow(color: Color(press ? #colorLiteral(red: 0.8195130229, green: 0.6300581098, blue: 0.8301144242, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+            .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.8195130229, green: 0.6300581098, blue: 0.8301144242, alpha: 1)), radius: 20, x: 20, y: 20)
+            .scaleEffect(tap ? 1.2 : 1)
+            .gesture(
+                LongPressGesture().updating($tap) { currentState, gestureState, transaction in
+                    gestureState = currentState
+                    
+                }
+                .onEnded { value in
+                    self.press.toggle()
+                }
+        )
     }
 }
